3c1f11678cf2f0d0f9f922f477f64a878c8ed5b0
FarzamAlam <farzamcse@gmail.com>
2016-10-22 00:45:14 +0530

    Add files via upload
    This is a Hello World Program . It uses Acm library .

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import acm.graphics.*;
+import acm.program.*;
+
+public class HelloWorld extends GraphicsProgram{
+
+	public void run()
+	{
+		add(new GLabel("Hello World !") , 100, 75);
+		
+	}
+	
+}
